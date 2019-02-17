c04538e1b574945aabdcb55db4096e46700d5a33
Oleksandr Maksymenko <mrsums2@gmail.com>
2019-02-17 23:34:10 +0200

    git add

---
+++ hw1/src/com/epam/homework1/WriteEpam.java
@@ -0,0 +1,16 @@
+package com.epam.homework1;
+
+public class WriteEpam {
+    public static void main (String[] arg){
+        System.out.println("* * * *   * * * *       *        *       *");
+        System.out.println();
+        System.out.println("*         *     *     *   *      * *   * *");
+        System.out.println();
+        System.out.println("* *       * * * *    *     *     *   *   *");
+        System.out.println();
+        System.out.println("*         *         * * * * *    *       *");
+        System.out.println();
+        System.out.println("* * * *   *        *         *   *       *");
+    }
+
+}
